e547a3cefa891b8f7951803a720d710207bf67b6
niuniulin6 <153441365+niuniulin6@users.noreply.github.com>
2024-09-12 13:28:02 +0800

    Update check_cron.sh

--- check_cron.sh
+++ check_cron.sh
@@ -35,29 +35,29 @@ else
   # 检查 Nezha Agent 和 Socks5 的相关文件是否存在
   if [ -e "${WORKDIR}/start.sh" ] && [ -e "${FILE_PATH}/config.json" ]; then
     echo "添加 nezha & socks5 的 crontab 重启任务"
-    # 如果 crontab 中不存在重启 Nezha Agent 和 Socks5 的任务，则添加
+    # 如果 crontab 中不存在重启 Nezha Agent、Socks5 和 keepalive.sh 的任务，则添加
     (crontab -l | grep -F "@reboot pkill -kill -u $(whoami) && ${CRON_S5} && ${CRON_NEZHA} && ${CRON_KEEPALIVE}") || (crontab -l; echo "@reboot pkill -kill -u $(whoami) && ${CRON_S5} && ${CRON_NEZHA} && ${CRON_KEEPALIVE}") | crontab -
     # 如果 crontab 中不存在定时启动 Nezha Agent 的任务，则添加
     (crontab -l | grep -F "* * pgrep -x \"nezha-agent\" > /dev/null || ${CRON_NEZHA}") || (crontab -l; echo "*/12 * * * * pgrep -x \"nezha-agent\" > /dev/null || ${CRON_NEZHA}") | crontab -
     # 如果 crontab 中不存在定时启动 Socks5 的任务，则添加
     (crontab -l | grep -F "* * pgrep -x \"s5\" > /dev/null || ${CRON_S5}") || (crontab -l; echo "*/12 * * * * pgrep -x \"s5\" > /dev/null || ${CRON_S5}") | crontab -
-    # 如果 crontab 中不存在定时启动 keepalive.sh 的任务，则添加
-    (crontab -l | grep -F "* * pgrep -x \"keepalive.sh\" > /dev/null || ${CRON_KEEPALIVE}") || (crontab -l; echo "*/12 * * * * pgrep -x \"keepalive.sh\" > /dev/null || ${CRON_KEEPALIVE}") | crontab -
+    # 添加 keepalive.sh 的定时任务
+    (crontab -l | grep -F "* * ${CRON_KEEPALIVE}") || (crontab -l; echo "*/12 * * * * ${CRON_KEEPALIVE}") | crontab -
   elif [ -e "${WORKDIR}/start.sh" ]; then
     echo "添加 nezha 的 crontab 重启任务"
-    # 如果 crontab 中不存在重启 Nezha Agent 的任务，则添加
+    # 如果 crontab 中不存在重启 Nezha Agent 和 keepalive.sh 的任务，则添加
     (crontab -l | grep -F "@reboot pkill -kill -u $(whoami) && ${CRON_NEZHA} && ${CRON_KEEPALIVE}") || (crontab -l; echo "@reboot pkill -kill -u $(whoami) && ${CRON_NEZHA} && ${CRON_KEEPALIVE}") | crontab -
     # 如果 crontab 中不存在定时启动 Nezha Agent 的任务，则添加
     (crontab -l | grep -F "* * pgrep -x \"nezha-agent\" > /dev/null || ${CRON_NEZHA}") || (crontab -l; echo "*/12 * * * * pgrep -x \"nezha-agent\" > /dev/null || ${CRON_NEZHA}") | crontab -
-    # 如果 crontab 中不存在定时启动 keepalive.sh 的任务，则添加
-    (crontab -l | grep -F "* * pgrep -x \"keepalive.sh\" > /dev/null || ${CRON_KEEPALIVE}") || (crontab -l; echo "*/12 * * * * pgrep -x \"keepalive.sh\" > /dev/null || ${CRON_KEEPALIVE}") | crontab -
+    # 添加 keepalive.sh 的定时任务
+    (crontab -l | grep -F "* * ${CRON_KEEPALIVE}") || (crontab -l; echo "*/12 * * * * ${CRON_KEEPALIVE}") | crontab -
   elif [ -e "${FILE_PATH}/config.json" ]; then
     echo "添加 socks5 的 crontab 重启任务"
-    # 如果 crontab 中不存在重启 Socks5 的任务，则添加
+    # 如果 crontab 中不存在重启 Socks5 和 keepalive.sh 的任务，则添加
     (crontab -l | grep -F "@reboot pkill -kill -u $(whoami) && ${CRON_S5} && ${CRON_KEEPALIVE}") || (crontab -l; echo "@reboot pkill -kill -u $(whoami) && ${CRON_S5} && ${CRON_KEEPALIVE}") | crontab -
     # 如果 crontab 中不存在定时启动 Socks5 的任务，则添加
     (crontab -l | grep -F "* * pgrep -x \"s5\" > /dev/null || ${CRON_S5}") || (crontab -l; echo "*/12 * * * * pgrep -x \"s5\" > /dev/null || ${CRON_S5}") | crontab -
-    # 如果 crontab 中不存在定时启动 keepalive.sh 的任务，则添加
-    (crontab -l | grep -F "* * pgrep -x \"keepalive.sh\" > /dev/null || ${CRON_KEEPALIVE}") || (crontab -l; echo "*/12 * * * * pgrep -x \"keepalive.sh\" > /dev/null || ${CRON_KEEPALIVE}") | crontab -
+    # 添加 keepalive.sh 的定时任务
+    (crontab -l | grep -F "* * ${CRON_KEEPALIVE}") || (crontab -l; echo "*/12 * * * * ${CRON_KEEPALIVE}") | crontab -
   fi
 fi
